d95701ffdb9c478e8fbe6436375bfd51cab6a6c4
StuffCod3 <evgeny.mell@yandex.ru>
2024-01-21 20:03:06 +0300

    Initial commit

---
+++ service-a/src/test/java/ru/stuff/servicea/ServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stuff.servicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
