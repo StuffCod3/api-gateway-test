bd8f426ea25872111f4b6599fecc2cad220baa09
StuffCod3 <evgeny.mell@yandex.ru>
2024-01-21 22:15:01 +0300

    Configuring Routes in RouteConfig

---
+++ api-gateway/src/main/java/ru/stuff/apigateway/route/RouteConfig.java
@@ -0,0 +1,26 @@
+package ru.stuff.apigateway.route;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        "service-a",
+                        p -> p
+                                .path("/service-a/**")
+                                .uri("lb://service-a"))
+                .route(
+                        "service-b",
+                        p -> p
+                                .path("/service-b/**")
+                                .uri("lb://service-b"))
+                .build();
+    }
+}
